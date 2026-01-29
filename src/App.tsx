24d420b97efec1440823a5648cf894d146fa24f9
Reid Herrera <reidherrerabusiness@gmail.com>
2026-01-29 09:38:19 -0600

    building our form component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useRef } from "react";
 import Input from "./Components/Input";
+import Form from "./Components/Form";
+import Button from "./Components/Button";
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  // const input = useRef<HTMLInputElement>(null);
 
   return (
     <main>
-      <Input label="Test" id="test" ref={input} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }

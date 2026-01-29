360aced080314813c264ee83b65b34ddb0a7293f
Reid Herrera <reidherrerabusiness@gmail.com>
2026-01-29 11:03:38 -0600

    completed the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { useRef } from "react";
 import Input from "./Components/Input";
-import Form from "./Components/Form";
+import Form, { type FormHandle } from "./Components/Form";
 import Button from "./Components/Button";
+import { useRef } from "react";
 
 function App() {
   // const input = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
     console.log(extractedData);
+    customForm.current?.clear();
   }
 
   return (
